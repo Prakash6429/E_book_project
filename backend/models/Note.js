fd4584004d2e82228eddca716d8a260b611a565e
Prakash Raj <rajprakash19.may@gmail.com>
2022-06-29 14:20:44 +0530

    commit

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
     title:{
         type: String,
         required: true
@@ -14,9 +19,9 @@ const NotesSchema = new Schema({
         default: "General"
     },
     date:{
-        type: date,
+        type: Date,
         default: Date.now
-    }
+    },
     });
 
     module.exports = mongoose.model('notes', NotesSchema);
